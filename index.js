263a53d80a79588fe06abab45c32eb3f5a440065
Kerry Guarino <kerry.guarino@gmail.com>
2023-04-29 10:33:10 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+
+function returnFirstTwoDrivers(array) {
+   let newArray = [...array];
+   newArray.splice(2)
+   return newArray;
+}
+
+function returnLastTwoDrivers(array) {
+   let newArray = [...array];
+   return (newArray.slice(2));
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(value) {
+   return function (fare) {
+      return fare * value
+   }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, functionHere){
+   return functionHere(arrayOfDrivers)
+}
+
